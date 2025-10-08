5e205a49b949e67c81d607ab784b5f72eddedfd4
vishwasrao <vishwasrao.salunkhe@gmail.com>
2025-10-08 12:53:10 -0400

    Working state code

--- src/crd/crd.controller.ts
+++ src/crd/crd.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { CrdService } from './crd.service';
 
-@Controller()
+@Controller('services')
 export class CrdController {
     constructor(private readonly crdService: CrdService) { }
 
     // CRD endpoint - accepts a POST body (not used) and returns a CDS Hooks-style info card
-    @Post('crd')
+    // full path: POST /services/crd-service
+    @Post('crd-service')
     crd(@Body() _body: unknown) {
         return this.crdService.getCrdCard();
     }
